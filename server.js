6a63054212d581d55850131f8a2428a4c497afb4
Matias <64437245+josematias92@users.noreply.github.com>
2025-02-27 09:31:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -350,6 +350,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public" ,'index.html'));
 });
 
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname,'indexV3.html'));
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
